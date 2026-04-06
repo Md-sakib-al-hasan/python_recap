e87b18e4934e08e0f3299c7af611a4d62e560393
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2026-04-06 16:19:28 +0600

    feat: add adder function and demonstrate usage with print statement

--- functions.py
+++ functions.py
@@ -1,4 +1,17 @@
 def welcome_message(name):
     print(f"Welcome, {name}!")
 
-welcome_message("sakib")
+# welcome_message("sakib")
+
+
+
+def adder(a,b):
+
+    """
+    This function adds two numbers and returns the result.
+    """
+    sum = a + b
+    return sum
+
+print(adder(10,20) * 10)
+help
