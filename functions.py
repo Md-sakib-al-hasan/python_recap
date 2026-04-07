c7a3d59b47a005755c3a13186fa92b0db7200868
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2026-04-07 17:45:00 +0600

    add a simple funcitons

--- functions.py
+++ functions.py
@@ -1,17 +1,29 @@
-def welcome_message(name):
-    print(f"Welcome, {name}!")
+# def welcome_message(name):
+#     print(f"Welcome, {name}!")
 
-# welcome_message("sakib")
+# # welcome_message("sakib")
 
 
 
-def adder(a,b):
+# def adder(a,b):
 
-    """
-    This function adds two numbers and returns the result.
-    """
-    sum = a + b
-    return sum
+#     """
+#     This function adds two numbers and returns the result.
+#     """
+#     sum = a + b
+#     return sum
 
-print(adder(10,20) * 10)
-help
+# print(adder(10,20) * 10)
+# # help(adder)
+
+
+# def eq(*kwargs):
+#     print(kwargs)
+
+
+# eq(name="sakib", age=20, city="Dhaka")
+
+s_iter = iter([1,2,3,4,5])
+
+for i in s_iter:
+    print(i)
